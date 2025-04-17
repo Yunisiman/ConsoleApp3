b1cbfac8a86449588064512e370a11cfcf103d80
MATH RUDN <rudn.math@outlook.com>
2025-04-17 20:45:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        string[] names = { "Tom", "Alice", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+        var shortNames = names.Where(name => name.Length == 3);
+
+        Console.WriteLine("Имена длиной 3:");
+        foreach (var name in shortNames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
